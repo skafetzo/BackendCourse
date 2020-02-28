83f748cc7970f3bbbc74e8a078682309ae36eb39
skafetzo <skafetzog@cosmote.gr>
2020-02-28 14:27:20 +0200

    Solved Map problem

---
+++ src/com/hackerrank/data/structures/SolutiomMap.java
@@ -0,0 +1,59 @@
+package com.hackerrank.data.structures;
+
+import java.util.*;
+import java.io.*;
+
+    class SolutiomMap{
+
+
+        public static void main(String []args){
+
+            Map phoneBook = new HashMap();
+
+            Scanner in = new Scanner(System.in);
+            int n=in.nextInt();
+            in.nextLine();
+            for(int i=0;i<n;i++)
+            {
+                String name=in.nextLine();
+                int phone=in.nextInt();
+                int length = String.valueOf(phone).length();
+                if(length == 8 && checkString(name)) {
+                 phoneBook.put(name, phone);
+                }
+                in.nextLine();
+            }
+            while(in.hasNext())
+            {
+                String s=in.nextLine();
+                if(phoneBook.containsKey(s)) {
+                    System.out.println(s + "=" + phoneBook.get(s));
+                }else {
+                    System.out.println("Not found");
+
+                }
+            }
+        }
+
+        private static boolean checkString(String str) {
+            char ch;
+          //  boolean capitalFlag = false;
+            boolean lowerCaseFlag = false;
+
+            for(int i=0;i < str.length();i++) {
+                ch = str.charAt(i);
+                if (Character.isUpperCase(ch)) {
+                    return false;
+
+                } else if (Character.isLowerCase(ch)) {
+                    lowerCaseFlag = true;
+                }
+            }
+            return lowerCaseFlag;
+        }
+    }
+
+
+
+
+
