f1e0cab86a0e62130dfeef30a5945d78f354b217
George Andrinopoulos <geoandri@gmail.com>
2020-02-28 10:16:52 +0200

    Solved Generics problem

---
+++ src/com/hackerrank/data/structures/SolutionGenerics.java
@@ -0,0 +1,50 @@
+package com.hackerrank.data.structures;
+
+import java.lang.reflect.Method;
+
+
+    public class SolutionGenerics {
+
+
+
+        public static void main( String args[] ) {
+            Printer myPrinter = new Printer();
+            Integer[] intArray = { 1, 2, 3 };
+            String[] stringArray = {"Hello", "World"};
+            myPrinter.printArray(intArray);
+            myPrinter.printArray(stringArray);
+            int count = 0;
+
+            for (Method method : Printer.class.getDeclaredMethods()) {
+                String name = method.getName();
+
+                if(name.equals("printArray"))
+                    count++;
+            }
+
+            if(count > 1)System.out.println("Method overloading is not allowed!");
+
+        }
+
+
+      static   class Printer{
+
+            //Write your code here
+            private int length;
+            private Object[] myArray;
+
+
+            public Printer(){
+                myArray = new Object[length];
+            }
+
+            void printArray(Object[] myArray){
+
+                for(int i=0; i<myArray.length; i++){
+                    System.out.println(myArray[i]);
+                }
+            }
+
+        }
+    }
+
